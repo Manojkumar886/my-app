987cc8d1915d94fa6b89bc822356c972607109ab
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-07-26 20:52:32 +0530

    check01

---
+++ src/NewResource.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Recruite=()=>
+{
+    return(
+        <>
+        <h1>Welcome to react javascript</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>age</th>
+                </tr>
+                <tbody>
+                    <tr>
+                        <td>Madhu</td>
+                        <td>20</td>
+                    </tr>
+                </tbody>
+            </thead>
+        </table>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Recruite } from './NewResource';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
   <h1> Welcome to Zealous Academy</h1>
+  <Recruite/>
   </>
 );
